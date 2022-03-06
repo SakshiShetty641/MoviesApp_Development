d4af6f3e70ca26b0dc133c38bc87588c8c378a91
Sakshi Shetty <sakshishetty@Sakshis-MacBook-Pro.local>
2022-03-06 16:44:13 +0530

    Introducing application properties

--- src/main/java/com/thoughtworks/movies/MovieController.java
+++ src/main/java/com/thoughtworks/movies/MovieController.java
@@ -1,13 +1,17 @@
 package com.thoughtworks.movies;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MovieController {
 
+    @Value("${name}")
+    private String name;
+
     @GetMapping("/hello")
     public String display(){
-        return "Hello world!!!!, Spring App";
+        return "Hello world!!!!, Spring App "  + name;
     }
 }
